5a57078a2c3aea977db7df3feaec4e39a6888c97
MStein <47797106+mstein-ck@users.noreply.github.com>
2021-07-28 13:01:31 -0400

    Fix readme, some updates (#20)
    * some updates, fix readme
    
    * Update App.vue

--- test/src/App.vue
+++ test/src/App.vue
@@ -170,7 +170,7 @@ export default {
                 year: new Date().getFullYear(),
                 cardToken: "",
                 cvvToken: "",
-                name
+                name: ""
             },
             doSubmit: false
         };
