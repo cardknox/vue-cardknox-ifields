78a099a0557c572cc6e35973351402c4003b1057
mstein <mstein@cardknox.com>
2023-08-10 12:13:49 -0400

    Update server.js

--- test/src/server.js
+++ test/src/server.js
@@ -6,11 +6,15 @@ const axios = require('axios');
 
 app.use(express.json());
 
+const validPaths = ['verifyjson', 'gatewayjson'];
 app.post('/api/:path', async (req, res) => {
   console.log(req.body);
-  const gatewayResp = await axios.post('https://x1.cardknox.com/' + req.params.path, req.body);
-  console.log(gatewayResp.data);
-  res.json(gatewayResp.data);
+  if (validPaths.includes(req.params.path.toLowerCase())) {
+    const gatewayResp = await axios.post('https://x1.cardknox.com/' + req.params.path, req.body);
+    console.log(gatewayResp.data);
+    res.json(gatewayResp.data);
+  } else
+    res.sendStatus(400);
 });
 
 app.listen(port, () => {
