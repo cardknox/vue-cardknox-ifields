bd6a0eb3135fe9c0d47c95eef8c4d9f2d5429d6a
saltman-ck <saltman@cardknox.com>
2026-03-11 14:11:06 -0400

    Update constants.js

--- vue-cardknox-ifields/src/ifields/constants.js
+++ vue-cardknox-ifields/src/ifields/constants.js
@@ -1,30 +1,31 @@
-export const LOADED = "loaded";
-export const TOKEN = "token";
-export const PING = "ping";
-export const STYLE = "style";
-export const ERROR = "error";
-export const AUTO_SUBMIT = "autoSubmit";
-export const UPDATE = "update";
-export const GET_TOKEN = "getToken";
-export const FOCUS = "focus";
-export const UPDATE_ISSUER = "updateIssuer";
-export const INIT = "init";
-export const FORMAT = "format";
-export const SET_PLACEHOLDER = "setPlaceholder";
-export const CLEAR_DATA = "clearData";
-export const CARD_TYPE = "card";
-export const CVV_TYPE = "cvv";
-export const ACH_TYPE = "ach";
-export const SET_ACCOUNT_DATA = "setAccountData";
-export const ENABLE_LOGGING = "enableLogging";
-export const ENABLE_AUTO_SUBMIT = "enableAutoSubmit";
-export const ENABLE3DS = "enable3DS";
-export const DISABLE3DS = "disable3DS";
-export const AUTO_FORMAT_DEFAULT_SEPARATOR = " ";
-export const IFIELDS_VERSION = "3.4.2602.2001";
-export const PLUGIN_NAME = "vue-cardknox-ifields";
-export const IFIELD_ORIGIN = "https://cdn.cardknox.com";
+
+export const LOADED = 'loaded';
+export const TOKEN = 'token';
+export const PING = 'ping';
+export const STYLE = 'style';
+export const ERROR = 'error';
+export const AUTO_SUBMIT = 'autoSubmit';
+export const UPDATE = 'update';
+export const GET_TOKEN = 'getToken';
+export const FOCUS = 'focus';
+export const UPDATE_ISSUER = 'updateIssuer';
+export const INIT = 'init';
+export const FORMAT = 'format';
+export const SET_PLACEHOLDER = 'setPlaceholder';
+export const CLEAR_DATA = 'clearData';
+export const CARD_TYPE = 'card';
+export const CVV_TYPE = 'cvv';
+export const ACH_TYPE = 'ach';
+export const SET_ACCOUNT_DATA = 'setAccountData';
+export const ENABLE_LOGGING = 'enableLogging';
+export const ENABLE_AUTO_SUBMIT = 'enableAutoSubmit';
+export const ENABLE3DS = 'enable3DS';
+export const DISABLE3DS = 'disable3DS';
+export const AUTO_FORMAT_DEFAULT_SEPARATOR = ' ';
+export const IFIELDS_VERSION = '3.4.2602.2001';
+export const PLUGIN_NAME = 'vue-cardknox-ifields';
+export const IFIELD_ORIGIN = 'https://cdn.cardknox.com';
 export const THREEDS_ENVIRONMENT = {
-  Production: "production",
-  Staging: "staging",
+  Production: 'production',
+  Staging: 'staging',
 };
